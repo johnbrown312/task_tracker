4fadf520066753e8eb9b99b90a2438b4111b1964
johnbrown312 <licock13@gmail.com>
2026-06-18 21:36:22 +0300

    Refactor task management functions in task_tracker.py

--- task_tracker.py
+++ task_tracker.py
@@ -62,29 +62,6 @@ def list_tasks(status=None):
         tasks = filter(lambda x: x["status"] == status, tasks)
     for task in tasks:
         print(f'{task.get("id")}. {task.get("description")} [{task.get("status")}]')
+
  
- 
-# --- точка входа ---
-if len(sys.argv) < 2:
-    print("Использование: python task_tracker.py <команда>")
-    print("Команды: add, delete, update, mark-in-progress, mark-done, list")
-else:
-    action = sys.argv[1]
- 
-    if action == "add":
-        add_task()
-    elif action == "delete":
-        delete_task()
-    elif action == "update":
-        update_task()
-    elif action == "mark-in-progress":
-        mark_in_progress()
-    elif action == "mark-done":
-        mark_done()
-    elif action == "list":
-        if len(sys.argv) > 2:
-            list_tasks(sys.argv[2])
-        else:
-            list_tasks()
-    else:
-        print(f"Неизвестная команда: {action}")
+
